f6b0616fb488e5dcd4bc7aeb2845360bf4ea4867
Nelson Pino <devsoftware.etiquetas@gmail.com>
2023-11-30 15:32:15 -0400

    creacion del modelo de usuario.

---
+++ backend/models/user.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+const userSchema = new moongose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter your name'],
+        maxLength: [30, 'Your name cannot exceed 30 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'please enter your email'],
+        unique: true, 
+        validate: [validator.isEmail, 'Please enter valid email addres'] 
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter your password'],
+        minLength: [6, 'Your password must beb longer than 6 characters'],
+        select: false
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    createdAt: {
+         type: Date,
+         default: Date.now
+    },
+    resetPasswordToken: String,
+    resetPassWordExpire: Date
+});
+
+export default mongoose.model('User', userSchema);

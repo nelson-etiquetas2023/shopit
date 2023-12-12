32bfdb5bdfa9550324742e88e94f64bdf7246065
Nelson Pino <devsoftware.etiquetas@gmail.com>
2023-12-12 15:00:47 -0400

    Obtener order por Id y todas las ordenes de un usuario.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,7 +2,10 @@ import Order from '../models/order.js';
 import Product from '../models/product.js';
 import errorHandler from '../utils/errorHandler.js';
 import catchAsyncErrors from '../middlewares/catchAsyncErrors.js';
+import catchErrorAsync from '../middlewares/catchAsyncErrors.js';
+import ErrorHandler from '../utils/errorHandler.js';
 
+// New Order => /api/v1/order/new
 export const newOrder = catchAsyncErrors( async ( req, res, next ) => {
     const { 
         orderItems,
@@ -32,4 +35,32 @@ export const newOrder = catchAsyncErrors( async ( req, res, next ) => {
      });
 
 });
+
+// Get single Order => /api/v1/order/:id
+export const getSingleOrder = catchErrorAsync( async ( req, res, next ) => {
+
+   const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+   if (!order) {
+      return next(new ErrorHandler('No Order found with this ID', 404));
+   }
+
+   res.status(200).json({
+      success:true,
+      order
+   });
+
+});
+
+// Get logged in user orders => /api/v1/orders/me
+export const myOrders = catchAsyncErrors( async ( req, res, next) => {
+
+   const orders = await Order.find({ user: req.user.id });
+   res.status(200).json({
+      success:true,
+      orders
+   });
+    
+});
+
 
--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { newOrder } from '../controllers/orderController.js';
+import { 
+    newOrder,
+    getSingleOrder,
+    myOrders
+ } from '../controllers/orderController.js';
 import { isAuthenticateUser, authorizeRoles } from '../middlewares/auth.js'
 
 const router = express.Router();
 
 router.post('/order/new',isAuthenticateUser,authorizeRoles('admin'),newOrder);
+router.get('/order/:id',isAuthenticateUser,authorizeRoles('admin'),getSingleOrder);
+router.get('/orders/me',isAuthenticateUser,authorizeRoles('admin'),myOrders)
 
 export default router;

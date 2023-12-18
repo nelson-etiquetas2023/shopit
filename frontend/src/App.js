cfc11c9b8296348314defdfb63ac1c21bc8b593d
Nelson Pino <devsoftware.etiquetas@gmail.com>
2023-12-18 14:44:25 -0400

    Finalizacion del card-product y las rutas de paginas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import Header from './components/layout/header.js';
 import Footer from './components/layout/footer.js';
+import Home from './components/layout/home.js';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+      <Header/>
+        <Routes>    
+            <div className='container container-fluid'>
+              <Route path = "/" element={<Home/>}/>
+            </div>
+        </Routes>
+      <Footer/>
+      </div>  
+    </Router>  
   );
 }
 
